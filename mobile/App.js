1d88516e87c19c5a385b14736d5bd4d67b4b3dff
Thiago Faria Ramos <th.framos@gmail.com>
2020-03-29 19:37:30 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,16 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR'
+
+import React from 'react';
+
+
+
+import Routes from './src/routes';
+
+// div, span , header...
+// react native - não tem h1 , div - possui view, text - não possui semantica - atyle + objeto 
+export default function App() {
+  return (
+    <Routes />
+  );
+}
